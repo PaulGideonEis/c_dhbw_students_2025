ea37c3321c364682106d9769d34322e4e40ab7d0
Paul-Gideon Eis <inf25087@lehre.dhbw-stuttgart.de>
2025-12-15 16:08:43 +0100

    solved second task

--- 0002_VARIABLES/Time_Converter/Exercise/main.c
+++ 0002_VARIABLES/Time_Converter/Exercise/main.c
@@ -2,7 +2,20 @@
 
 int main()
 {
+    float milliseconds = 0.0F;
 
+    printf("Please enter a millisecond value: ");
+    scanf("%f", &milliseconds);
+
+    float seconds = milliseconds /1000.0f;
+    float minutes = seconds /60.0f;
+    float hours = minutes / 60.0f;
+    float days = hours / 24.0f;
+
+    printf("seconds: %f\n", seconds);
+    printf("minutes: %f\n", minutes);
+    printf("hours: %f\n", hours);
+    printf("days: %f\n", days);
 
     return 0;
 }
