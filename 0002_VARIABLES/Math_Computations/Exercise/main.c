14dae4145b9335866547c1c61b5d641b6adaa869
Paul-Gideon Eis <inf25087@lehre.dhbw-stuttgart.de>
2025-12-15 14:53:55 +0100

    solved first task

--- 0002_VARIABLES/Math_Computations/Exercise/main.c
+++ 0002_VARIABLES/Math_Computations/Exercise/main.c
@@ -2,15 +2,18 @@
 
 int main()
 {
+    int x = -2;
+    int y = -3;
+    int z1, z2, z3;
+
+    z1 = x^2 + y^2 - (x * y) + 2;
+    z2 = (x - y)^3 - 3;
+    z3 = (2 * x^3 - (1/2) * x^2 - x + 4) / y;
+
+    printf("%d \n%d \n%d",z1,z2,z3);
 
     return 0;
 }
-
-
-
-
-
-
 
 
 
