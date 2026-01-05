1d69205423ed273652e9fe7e25d313aeafea02aa
Paul-Gideon Eis <inf25087@lehre.dhbw-stuttgart.de>
2026-01-05 15:05:21 +0100

    3.1

---
+++ 0003_CONTROL_FLOW/CHAR_CONVERTER/Exercise/headerone.c
@@ -0,0 +1,42 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+    bool Is_Numeric(char character){
+    if (character > 47 && character < 58)
+    {
+        print}f("\n%c is nummeric", character);
+    }
+    bool Is_Upper_Case(char character){
+    if (character > 64 && character < 91)
+    {
+        printf("\n%c is uppercase", character);
+    }}
+
+    bool Is_Lower_Case(char character){
+    if (character > 96 && character < 123)
+        {
+            printf("\n%c is lowercase", character);
+        }}
+    bool Is_Alpha(char character){
+    if ((character > 64 && character < 91) || (character > 96 && character < 123) || (character > 47 && character < 58))
+        {
+            printf("\n%c is alphabetical", character);
+        }}
+    bool Is_Alpha_Numeric(char character){
+    if ((character > 64 && character < 91) || (character > 96 && character < 123))
+        {
+            printf("\n%c is alphanumerical", character);
+        }}
+    bool To_Upper_Case(char character){
+    if (character > 96 && character < 123)
+        {
+            character -= 32;
+            printf("\n%c now uppercase", character);
+        }}
+    bool ToLowerCase(char character){
+    if (character > 64 && character < 91)
+    {
+        character += 32;
+        printf("\n%c now lowercase", character);
+    }}
+
---
+++ 0003_CONTROL_FLOW/CHAR_CONVERTER/Exercise/lib.h
@@ -0,0 +1,14 @@
+#ifndef LIB_H
+#define LIB_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <headerone.h>
+
+bool Is_Numeric(char character)
+bool Is_Upper_Case(char character)
+bool Is_Lower_Case(char character)
+bool Is_Alpha(char character)
+bool Is_Alpha_Numeric(char character)
+bool To_Upper_Case(char character)
+bool ToLowerCase(char character)
